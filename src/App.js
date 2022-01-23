3bd6843e3b80dc395f63608a6a09936646574971
Petra <75530592+petraa97@users.noreply.github.com>
2022-01-23 15:00:07 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Children, Class, Function } from './korisnici';
 
 export default class App extends React.Component {
   state = {
-    users: [
+    korisnik: [
       { name: 'Iva', years: 30 },
       { name: 'Ivan', years: 35 },
       { name: 'Ivana', years: 25 },
@@ -12,16 +12,16 @@ export default class App extends React.Component {
   };
 
   render() {
-    const { users } = this.state;
+    const { korisnik } = this.state;
 
     return (
       <div>
         <h2>Korisnici: </h2>
 
-        <Function name={users[0].name} years={users[0].years} />
-        <Function name={users[1].name} years={users[1].years} />
-        <Class name={users[2].name} years={users[2].years} />
-        <Children name={users[2].name} years={users[2].years}>
+        <Function name={korisnik[0].name} years={korisnik[0].years} />
+        <Function name={korisnik[1].name} years={korisnik[1].years} />
+        <Class name={korisnik[2].name} years={korisnik[2].years} />
+        <Children name={korisnik[2].name} years={korisnik[2].years}>
         </Children>
       </div>
     );
